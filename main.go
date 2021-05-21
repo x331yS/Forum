ff4df6feab94682d0ec654496805acf647dbdca6
anatolethien <anatole.thien@ynov.com>
2021-05-21 12:48:37 +0200

    Static directory

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	app.Static("/", "./public")
 
 	app.Listen(":3000")
 }
