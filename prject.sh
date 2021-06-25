5c3fa932620af77e04af7c65f9f47e74eaae9f03
FLEJOSNE <florian.lejosne@ynov.com>
2021-06-25 16:46:55 +0200

    salut antoine

---
+++ prject.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+open https://github.com/anatolethien/forum
+open -a Firefox https://github.com/anatolethien/forum
--- public/static/js/size.js
+++
@@ -1,6 +0,0 @@
-if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
-    var script1= document.createElement('script');
-    script1.src = '../static/js/moove.js';
-    script1.type = 'text/javascript';
-    document.getElementsByTagName('body')[0].appendChild(script1);
-}
